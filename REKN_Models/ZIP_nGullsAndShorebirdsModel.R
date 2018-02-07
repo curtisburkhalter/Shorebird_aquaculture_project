e66e70bc699cd0c412ab46340c7cb57cc2470ee9
Curtis Burkhalter <curtisburkhalter@gmail.com>
2018-02-07 08:49:54 -0500

    Final updates to gulls and shorebirds model
    This file contains the final edits to the gulls and shorebirds model.

--- REKN_Models/ZIP_nGullsAndShorebirdsModel.R
+++ REKN_Models/ZIP_nGullsAndShorebirdsModel.R
@@ -178,6 +178,7 @@ out <- jags(data = data,
             n.cores=2)
 
 sink(file=here("Shorebird_aquaculture_project","OutputFiles","REKN","outputnGullsandShorebirdsModel.txt"))
+#out <- update(out,n.iter = 30000)
 out
 sink()
 
